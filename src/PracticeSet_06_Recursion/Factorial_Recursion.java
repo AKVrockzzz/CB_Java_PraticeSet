65819455d7caecec7ddb576abba9f02fa65fcbd2
AKVrockzzz <ajaykumarverma5549@gmail.com>
2024-06-30 13:25:00 +0530

    Basic Recursion Concept Practice
    Basic Recursion Concept Practice using Java
    Batch - Java DSA Monu Sir
    Coding Blocks

---
+++ src/PracticeSet_06_Recursion/Factorial_Recursion.java
@@ -0,0 +1,16 @@
+package PracticeSet_06_Recursion;
+
+public class Factorial_Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		System.out.println(fact(n));
+	}
+	public static int fact(int n) {
+		if(n == 0) return 1;
+		int fn = fact(n-1);
+		return fn*n;
+	}
+
+}
---
+++ src/PracticeSet_06_Recursion/First_Occ.java
@@ -0,0 +1,17 @@
+package PracticeSet_06_Recursion;
+
+public class First_Occ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2,4,3,5,6,3,7,3,56,3,11};
+		int item = 3;
+		System.out.println(Index(arr,item,0));
+	}
+	public static int Index(int[] arr,int item, int idx) {
+		if(idx == arr.length) return -1;
+		if(arr[idx] == item) return idx;
+		return Index(arr, item, idx+1);
+	}
+
+}

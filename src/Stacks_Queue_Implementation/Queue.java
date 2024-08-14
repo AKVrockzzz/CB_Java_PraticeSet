287f69a7b6042d63b495c6c5acb52e5335d173f8
AKVrockzzz <ajaykumarverma5549@gmail.com>
2024-08-14 20:11:47 +0530

    Stack and Queue Practice
    Stack and Queue Practice

--- src/Stacks_Queue_Implementation/Queue.java
+++ src/Stacks_Queue_Implementation/Queue.java
@@ -1,5 +1,44 @@
 package Stacks_Queue_Implementation;
 
 public class Queue {
-
+	private int[] arr;
+	private int front = 0;
+	private int size = 0;
+	
+	public Queue() {
+		arr = new int[5];
+	}
+	public Queue(int n) {
+		arr = new int[n];
+	}
+	
+	public boolean isempty() {
+		return size == 0;
+	}
+	
+	public void Equeue(int item) {
+		int idx = (front + size) % arr.length;
+		arr[idx] = item;
+		size++;
+	}
+	
+	public int getfront() {
+		int rv = arr[front];
+		return rv;
+	}
+	
+	public int Dequeue() {
+		int rv = arr[front];
+		front = (front + 1) % arr.length;
+		size--;
+		return rv;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isfull() {
+		return size == arr.length;
+	}
 }

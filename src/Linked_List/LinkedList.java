75301aba4d0e2a625e9e9600d6c8d61f50073e30
AKVrockzzz <ajaykumarverma5549@gmail.com>
2024-09-04 10:17:06 +0530

    LinkedList
    LinkedList Questions Concept

--- src/Linked_List/LinkedList.java
+++ src/Linked_List/LinkedList.java
@@ -25,6 +25,97 @@ public class LinkedList {
 		}
 	}
 	
+	public void AddLast(int item) {
+		if(size == 0) {
+			AddFirst(item);
+		}
+		else {
+			Node nn = new Node();
+			nn.data = item;
+			tail.next = nn;
+			tail = nn;
+			size++;
+		}
+	}
+	
+	public void AddatIndex(int item, int k) throws Exception{
+		if(k == 0) {
+			AddFirst(item);
+		}
+		else if(k == size) {
+			AddLast(item);
+		}
+		else {
+			Node nn = new Node();
+			nn.data = item;
+			Node k_1th = GetNode(k-1);
+			nn.next = k_1th.next;
+			k_1th.next = nn;
+			size++;
+			
+		}
+	}
+	
+	public int getFirst() {
+		return head.data;
+	}
+	
+	public int getLast() {
+		return tail.data;
+	}
+	
+	public int getAtIndex(int k) throws Exception{
+		return GetNode(k).data;
+	}
+	
+	
+	public int removeFirst() {
+		int rv = head.data;
+		if(size == 1) {
+			head = null;
+			tail = null;
+			size--;
+		}
+		else {
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+		}
+		size--;
+		return rv;
+	}
+	
+	public int removeLast()  throws Exception{
+		if(size == 1) {
+			return removeFirst();
+		}
+		else {
+			int rv = tail.data;
+			Node secondLast = GetNode(size-2);
+			secondLast.next =  null;
+			tail = secondLast;
+			size--;
+			return rv;
+		}
+	}
+	
+	public int removeAtIndex(int k) throws Exception {
+		if( k == 0) {
+			return removeFirst();
+		}
+		else if(k == size-1) {
+			return removeLast();
+		}
+		else {
+			Node k_1th = GetNode(k-1);
+			Node kth = k_1th.next;
+			k_1th.next = kth.next;
+			kth.next = null;
+			size--;
+			return kth.data;
+		}
+	}
+	
 	public void Display() {
 		 Node temp = head;
 		 while(temp != null) {
@@ -32,5 +123,16 @@ public class LinkedList {
 			 temp = temp.next;
 		 }
 		 System.out.println();
+	}
+	
+	private Node GetNode(int k) throws Exception{
+		if(k < 0 || k>=size) {
+			throw new Exception("Bklol Index range mein tho dede");
+		}
+		Node temp = head;
+		for(int i=0; i<k; i++) {
+			temp = temp.next;
+		}
+		return temp;
 	}
 }
--- src/Linked_List/LinkedList_Client.java
+++ src/Linked_List/LinkedList_Client.java
@@ -2,12 +2,28 @@ package Linked_List;
 
 public class LinkedList_Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		LinkedList ll = new LinkedList();
 		ll.AddFirst(10);
 		ll.AddFirst(20);
 		ll.AddFirst(30);
+		ll.AddLast(40);
+		ll.AddatIndex(-20, 2);
+		ll.AddFirst(23);
+		ll.AddFirst(54);
+		ll.AddFirst(12);
+		ll.AddLast(90);
+		ll.Display();
+		
+//		System.out.println(ll.getFirst());
+//		System.out.println(ll.getLast());
+//		System.out.println(ll.getAtIndex(2));
+		System.out.println(ll.removeFirst());
+		ll.Display();
+		System.out.println(ll.removeLast());
+		ll.Display();
+		System.out.println(ll.removeAtIndex(4));
 		ll.Display();
 	}
 
